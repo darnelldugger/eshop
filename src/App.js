1068e2159b4340d7cf13b2156b2ce64f8d34d230
Darnell Dugger <darnelldugger@gmail.com>
2022-04-29 14:57:19 -0400

    add links and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Checkout from './components/Checkout';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/checkout' exact element={<Checkout />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
