01f7cf695b1f0da8dd7c4c9377cc4623952ab440
Darnell Dugger <darnelldugger@gmail.com>
2022-04-29 12:06:09 -0400

    add header component ad import to app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-
 import './App.css';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import StorefrontIcon from '@mui/icons-material/Storefront';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className='App'>
-      <div className="header__logo">
-        <StorefrontIcon className='headerLogoImage' fontSize='large'/>
-        <h2 className='headerLogoTitle'>eShop</h2>
-      </div>
-      <div className="header__search">
-
-      </div>
-      <div className="header__nav">
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Hello Guest</span>
-          <span className="nav__itemLineTwo">Sign In</span>
-        </div>
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Your</span>
-          <span className="nav__itemLineTwo">Shop</span>
-        </div>
-        <div className="nav__item">
-          <ShoppingBasketIcon fontSize='large' />
-          <span className="nav__itemLineTwo">0</span>
-        </div>
-      </div>
+      <Header />
     </div>
   );
 }
