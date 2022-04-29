478341afb3ade6de4999fcd7c765ee557eece568
Darnell Dugger <darnelldugger@gmail.com>
2022-04-29 12:56:34 -0400

    create home component

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+  return (
+    <div>
+      <div className="home">
+        <div className="home__container">
+          <div className="home__row">
+            
+          </div>
+          <div className="home__row">
+
+          </div>
+          <div className="home__row">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
